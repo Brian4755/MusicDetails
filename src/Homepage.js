94bd2d5dacf0baaddb730a7a7379cd59b90a142a
Brian4755 <brian.mccune1@yahoo.com>
2023-05-17 02:11:50 -0500

    api service full title display with lyrics

--- src/Homepage.js
+++ src/Homepage.js
@@ -9,13 +9,14 @@ const Homepage = () => {
   const scrollToRef = useRef();
 
   useEffect(() => {
-    music.lyrics && scrollToRef.current.scrollIntoView({ behavior: "smooth" })
+    music[1] && scrollToRef.current.scrollIntoView({ behavior: "smooth" })
   });
 
   const handleMusicSearch = async FormData => {
     const musicResults = await musicSearch(FormData)
     setMusic(musicResults)
-    console.log(musicResults.lyrics.lyrics.body.html)
+    console.log(musicResults)
+    console.log(musicResults[0].lyrics.lyrics.body.html)
   }
 
 
@@ -24,7 +25,10 @@ const Homepage = () => {
       <h1>Type the Song Name for Lyrics</h1>
       <div className='musicSearch' >
         <MusicSearch handleMusicSearch = {handleMusicSearch}/>
-        <div ref={scrollToRef} className='musicLyrics' dangerouslySetInnerHTML={{__html: music.lyrics?.lyrics.body.html}} />
+        <div ref={scrollToRef}>
+          <h2>{music[1]?.full_title}</h2>
+          <div className='musicLyrics' dangerouslySetInnerHTML={{__html: music[0]?.lyrics.lyrics.body.html}} />
+        </div>
       </div>
     </>
    )
--- src/services/musicService.js
+++ src/services/musicService.js
@@ -4,10 +4,8 @@ export async function musicSearch(formData){
   console.log(formData.query)
   const res = await fetch(`${BASE_URL}/api/music?q=${formData.query}`)
   const data = await res.json()
-  // return console.log(data.hits[0].result.id)
   console.log(data)
-  // return musicLyrics(data)
-  const resID = await fetch(`${BASE_URL}/api/id?id=${data}`)
+  const resID = await fetch(`${BASE_URL}/api/id?id=${data.id}`)
   const lyricData = await resID.json()
-  return lyricData
+  return [lyricData, data]
 }
